94a434df6579311c4115154cc3617f441211a544
Praveen-Sasi <praveenmp471@gamil.com>
2023-11-23 23:05:43 +0530

    new one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppNameComponent } from './app-name/app-name.component';
+import { LoginComponent } from './user/login/login.component';
+import { LogoutComponent } from './user/logout/logout.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ChangePasswordComponent } from './user/change-password/change-password.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNameComponent
+    AppNameComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    ChangePasswordComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
